470ff5031b3d41075d84f48f86f4f5662892fd72
MeshachB <MeshachCBrown@gmail.com>
2026-05-11 21:40:44 -0400

    Add authentication system and Listing CRUD create feature

--- listings/urls.py
+++ listings/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('signup/', views.signup, name='signup'),
+    path('create/', views.create_listing, name='create'),
 ]
--- listings/views.py
+++ listings/views.py
@@ -1,4 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
 
 def home(request):
     return render(request, 'base.html')
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+
+    return render(request, 'signup.html', {'form': form}) 
+
+    from .models import Listing
+from .forms import ListingForm
+from django.contrib.auth.decorators import login_required
+
+@login_required
+def create_listing(request):
+    if request.method == 'POST':
+        form = ListingForm(request.POST)
+        if form.is_valid():
+            listing = form.save(commit=False)
+            listing.owner = request.user
+            listing.save()
+            return redirect('home')
+    else:
+        form = ListingForm()
+
+    return render(request, 'create_listing.html', {'form': form})
